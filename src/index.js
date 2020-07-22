3ebaae79cee699b2daa88d6ac4c7c07b38d90e03
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-22 11:48:14 -0500

    added sand ground to scene

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import Phaser from "phaser";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
-import playGame from "./phaser/scene";
+import Phaser from 'phaser';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import playGame from './phaser/scene';
 
 export const config = {
     type: Phaser.AUTO,
-    parent: "phaser",
+    parent: 'phaser',
+    backgroundColor: '#1b1464',
     width: 800,
     height: 600,
+    physics: {
+        default: 'matter',
+        matter: {
+        }
+    },
     scene: playGame
 };
 
 const game = new Phaser.Game(config);
 
-ReactDOM.render(<App />, document.getElementById("root") || document.createElement("div"));
+ReactDOM.render(<App />, document.getElementById('root') || document.createElement('div'));
+
--- src/phaser/scene.js
+++ src/phaser/scene.js
@@ -1,21 +1,37 @@
-import Phaser from "phaser";
-import logoImg from "../assets/logo.png";
+import Phaser from 'phaser';
+import GroundSand from '../assets/sprites/groundSand.png';
 
 class playGame extends Phaser.Scene {
   constructor() {
-    super("PlayGame");
+    super('PlayGame');
   }
   preload() {
-    this.load.image("logo", logoImg);
+    // assets to prepare
+    this.load.image('wall', 'assets/wall.png');
+    this.load.image('groundSand', GroundSand);
   }
   create() {
-    const logo = this.add.image(400, 150, "logo");
 
+    // mouse control group
+    this.matter.add.mouseSpring({ length: 1, stiffness: 0.6, collisionFilter: { group: canDrag } });
+    var canDrag = this.matter.world.nextGroup();
+
+    // world items
+    // main wall building block
+    const wall = this.matter.add.image(100, 100, 'wall', null, 
+    { chamfer: 16, density: 30, friction: 0.9, frictionStatic: 0.75}).setCollisionGroup(canDrag);
+    // the ground that the player sees
+    const groundSand = this.add.image(0, 450, 'groundSand').setOrigin(0, 0);
+
+
+
+
+    // tween effects
     this.tweens.add({
-      targets: logo,
+      targets: wall,
       y: 450,
       duration: 2000,
-      ease: "Power2",
+      ease: 'power2',
       yoyo: true,
       loop: -1
     });
