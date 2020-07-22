89da4de0fe761efe0d1925bb5f453cee77cc2909
Joshua Edwards <joshuaedwards@elizabeths-imac.attlocal.net>
2020-07-22 10:55:50 -0500

    project setup with react and phaser3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
 	render() {
 		return (
 			<div style={{ textAlign: "center" }}>
-				<h1>Hello World</h1>
+				<h1>Quicksand Castle</h1>
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -4,19 +4,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
 import playGame from "./phaser/scene";
 
-//console.log(App);
-
 export const config = {
-  type: Phaser.AUTO,
-  parent: "phaser",
-  width: 800,
-  height: 600,
-  scene: playGame
+    type: Phaser.AUTO,
+    parent: "phaser",
+    width: 800,
+    height: 600,
+    scene: playGame
 };
 
 const game = new Phaser.Game(config);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root") || document.createElement("div")
-);
+ReactDOM.render(<App />, document.getElementById("root") || document.createElement("div"));
